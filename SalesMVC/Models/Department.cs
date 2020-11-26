f7fbdc2332c9bd2e243bf689d549f667fa642629
Junior <uidj7882@contiwan.com>
2020-11-26 16:03:38 -0300

    Other entities - Aula 251

--- SalesMVC/Models/Department.cs
+++ SalesMVC/Models/Department.cs
@@ -1,8 +1,28 @@
-﻿namespace SalesMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesMVC.Models
 {
     public class Department
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int iD, string name, ICollection<Seller> sellers)
+        {
+            ID = iD;
+            Name = name;
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
