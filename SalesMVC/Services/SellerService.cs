11d855a4db88a7405b36fb3a9407505c4a393098
Dorival Junior <dasj.junior@gmail.com>
2020-12-03 08:05:07 -0300

    Create New Form - Aula 255

---
+++ SalesMVC/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesMVC.Models;
+
+namespace SalesMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesMVCContext _context;
+
+        public SellerService(SalesMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+    }
+}
--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesMVC.Models;
 using SalesMVC.Data;
+using SalesMVC.Services;
 
 namespace SalesMVC
 {
@@ -41,7 +42,7 @@ namespace SalesMVC
                     options.UseMySql(Configuration.GetConnectionString("SalesMVCContext"), builder => builder.MigrationsAssembly("SalesMVC")));
 
             services.AddScoped<SeedingService>();
-
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
