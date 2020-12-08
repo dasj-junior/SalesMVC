1822af73915d29ee383564e514be15d3e0cce6a5
dasj-junior <dasj.junior@gmail.com>
2020-12-08 08:00:37 -0300

    App locale, number and date format - Aula 262

--- SalesMVC/Models/Seller.cs
+++ SalesMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesMVC.Models
@@ -8,8 +9,14 @@ namespace SalesMVC.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [DisplayFormat(DataFormatString = "R$ {0:F2}")]
+        [Display(Name = "Base Salary")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentID { get; set; }
